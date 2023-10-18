c664a7bfb81487b2ea30019d5be50791e3c0d6e8
jasusol <jsusol@g.clemson.edu>
2023-10-18 12:59:21 -0400

    scheduler page additions

--- src/pages/SchedulerPage.tsx
+++ src/pages/SchedulerPage.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Grid, TextField, Typography } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import React from "react";
 
 function SchedulerPage() {
   return (
-    <div>SchedulerPage</div>
-  )
+    <>
+      <Typography variant="h3" textAlign="center" sx={{ marginTop: 3 }}>
+        Scheduler
+      </Typography>
+
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "50vh" }}
+      >
+        <Typography>Event Title</Typography>
+        <TextField sx={{ marginBottom: 2 }}></TextField>
+
+        <Typography>Event Date</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker />
+        </LocalizationProvider>
+
+        <Typography sx={{ marginTop: 2 }}>Event Description</Typography>
+        <TextField sx={{ marginBottom: 2 }}></TextField>
+
+        
+      </Grid>
+    </>
+  );
 }
 
-export default SchedulerPage
+export default SchedulerPage;
